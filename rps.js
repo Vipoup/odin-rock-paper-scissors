af71f0abf1eaa12a7e29191b4e00e72720b32c5b
Vipoup <60632610+Vipoup@users.noreply.github.com>
2024-02-19 16:39:27 -0500

    Add player vs computer function

--- rps.js
+++ rps.js
@@ -8,9 +8,33 @@ function getComputerChoice(){
             return "paper";
             break;
         case 2:
-            return "scissor";
+            return "scissors";
             break; 
     }
 }
 
-// console.log(getComputerChoice())
+function playRound(playerChoice, computerChoice){
+    playerChoice = playerChoice.toLowerCase();
+    if(playerChoice === "rock" || 
+    playerChoice === "paper" ||
+    playerChoice === "scissors"){
+        //continue
+    }else{
+        return "Invalid choice. Choose [rock, paper, scissors]";
+    }
+
+    if(playerChoice === "rock" && computerChoice === "rock" ||
+    playerChoice === "scissors" && computerChoice === "scissors" ||
+    playerChoice === "paper" && computerChoice === "paper"){
+
+        return `You:${playerChoice} || Computer: ${computerChoice} || Tied`;
+    } else if (playerChoice === "rock" && computerChoice === "scissors" ||
+    playerChoice === "scissors" && computerChoice === "paper" ||
+    playerChoice === "paper" && computerChoice === "rock"){
+
+        return `You:${playerChoice} || Computer: ${computerChoice} || You have Won!`;
+    } else {
+
+        return `You:${playerChoice} || Computer: ${computerChoice} || You have lost.`;
+    }
+}
