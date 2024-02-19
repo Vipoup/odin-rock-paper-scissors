e6c145612f700ffe2ee1efd4494777bca8c6ee6d
Vipoup <60632610+Vipoup@users.noreply.github.com>
2024-02-19 17:07:12 -0500

    Create a game that keeps track of score and rework
    Create getInput to clean user input
    Change playRound to return values that can make
    decisions.

--- rps.js
+++ rps.js
@@ -13,28 +13,74 @@ function getComputerChoice(){
     }
 }
 
-function playRound(playerChoice, computerChoice){
-    playerChoice = playerChoice.toLowerCase();
-    if(playerChoice === "rock" || 
-    playerChoice === "paper" ||
-    playerChoice === "scissors"){
-        //continue
-    }else{
-        return "Invalid choice. Choose [rock, paper, scissors]";
+function getInput(){
+    playerChoice = prompt("Choose [rock, paper, scissors].");
+    check = true;
+    while(check){
+        playerChoice = playerChoice.toLowerCase();
+        if(playerChoice === "rock" || 
+        playerChoice === "paper" ||
+        playerChoice === "scissors"){
+            check = false;
+            return playerChoice;
+        }else{
+            playerChoice = prompt("Invalid choice. Choose [rock, paper, scissors]");
+        }
     }
+}
 
+function playRound(playerChoice, computerChoice){
     if(playerChoice === "rock" && computerChoice === "rock" ||
     playerChoice === "scissors" && computerChoice === "scissors" ||
     playerChoice === "paper" && computerChoice === "paper"){
 
-        return `You:${playerChoice} || Computer: ${computerChoice} || Tied`;
+        return 0;
     } else if (playerChoice === "rock" && computerChoice === "scissors" ||
     playerChoice === "scissors" && computerChoice === "paper" ||
     playerChoice === "paper" && computerChoice === "rock"){
 
-        return `You:${playerChoice} || Computer: ${computerChoice} || You have Won!`;
+        return 1;
     } else {
 
-        return `You:${playerChoice} || Computer: ${computerChoice} || You have lost.`;
+        return 2;
+    }
+}
+
+function playGame(){
+    let playerChoice, computerChoice;
+    let roundWinner;
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for(let i = 0; i < 5; i++){
+        playerChoice = getInput();
+        computerChoice = getComputerChoice();
+        roundWinner = playRound(playerChoice, computerChoice);
+
+        switch(roundWinner){
+            case 0:
+                console.log(`You:${playerChoice} || Computer: ${computerChoice} || Tied`);
+                break;
+            case 1:
+                console.log(`You:${playerChoice} || Computer: ${computerChoice} || You have Won!`);
+                playerScore++;
+                break;
+            case 2:
+                console.log(`You:${playerChoice} || Computer: ${computerChoice} || You have lost.`);
+                computerScore++;
+                break;
+        }
+
+        console.log(`Current Score: Player-${playerScore}, Computer-${computerScore}`);
+    }
+
+    console.log(`Final Score: Player-${playerScore}, Computer-${computerScore}`);
+
+    if(playerScore > computerScore){
+        return "You win!"
+    }else if(playerScore < computerScore){
+        return "You lose."
+    }else{
+        return "Tied."
     }
 }
